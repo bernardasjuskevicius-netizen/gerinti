8c4c528315295fd9260c2672a719a3d49d5fdd88
bernardasjuskevicius-netizen <bernardas.juskevicius@gmail.com>
2026-02-27 10:00:05 +0200

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -3,10 +3,11 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("auth");
   eleventyConfig.addPassthroughCopy("_redirects");
+  eleventyConfig.addPassthroughCopy("emails"); // kad invite.html būtų publikuotas, bet ne-renderinamas
 
   return {
     dir: { input: ".", includes: "_includes", output: "_site" },
     markdownTemplateEngine: "njk",
-    htmlTemplateEngine: "njk",
+    htmlTemplateEngine: false, // SVARBIAUSIA: .html failų nerenderinam (kopijuojam kaip yra)
   };
 };
